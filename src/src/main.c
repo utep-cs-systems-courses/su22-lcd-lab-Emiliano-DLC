dd424e8b1fe42e3dcdc00307c5c550d6a3ba2e35
Emiliano de la Cruz <Emiliano_r_d_l_c@outlook.com>
2022-08-01 18:10:01 -0600

    Moved and created necessari files

---
+++ src/src/main.c
@@ -0,0 +1,30 @@
+#include <msp430.h>
+#include <libTimer.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+
+#define LED BIT6		/* note that bit zero req'd for display */
+
+void main()
+{
+  
+  P1DIR |= LED;		/**< Green led on when CPU on */
+  P1OUT |= LED;
+  configureClocks();
+  lcd_init();
+  switch_init();
+  
+  enableWDTInterrupts();      /**< enable periodic interrupt */
+  or_sr(0x8);	              /**< GIE (enable interrupts) */
+  
+  clearScreen(BG_COLOR);
+  while (1) {			/* forever */
+    if (redrawScreen) {
+      redrawScreen = 0;
+      update_shape();
+    }
+    P1OUT &= ~LED;	/* led off */
+    or_sr(0x10);	/**< CPU OFF */
+    P1OUT |= LED;	/* led on */
+  }
+}
