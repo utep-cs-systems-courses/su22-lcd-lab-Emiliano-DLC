606edddf0638896bd6a888837651a40ccd9fef40
Emiliano de la Cruz <Emiliano_r_d_l_c@outlook.com>
2022-08-01 22:34:20 -0600

    WIP

--- src/src/lcd.c
+++ src/src/lcd.c
@@ -4,3 +4,19 @@
 #include "lcddraw.h"
 #include "switches.h"
 #include "lcd.h"
+
+
+void draw_SQ()
+{
+  for (unsigned char col = 0; col < screenWidth; col++)
+    {
+      for(unsigned char row = 0; row < screenHeight; row++)
+	{
+	  if(col % 2 == 0)
+	    {
+	      fillRectangle(col, row, 1, 1, COLOR_PINK);
+	    }
+	  else
+	    fillRectangle(col, row, 1, 1, COLOR_BLUE);
+	}
+}
