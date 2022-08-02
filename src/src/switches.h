98def067381f12a21f57ea7fe0db6e6b98279554
Emiliano de la Cruz <Emiliano_r_d_l_c@outlook.com>
2022-08-01 18:36:58 -0600

    added header files

---
+++ src/src/switches.h
@@ -0,0 +1,19 @@
+#ifndef switches_included
+#define switches_included
+
+//P2DIR
+//define buttons on external board (p2)
+#define SW1 1 
+#define SW2 2 
+#define SW3 4 
+#define SW4 8 
+#define SWITCHES (SW1 | SW2 | SW3 | SW4) //Switches for external board (p2)
+
+//P1DIR
+//#define SW0 BIT4		/* switch1 is p1.3 */
+//#define P1SWITCHES(SW0)		/* only 1 switch on this board */
+
+void switch_init();
+void switch_interrupt_handler();
+
+extern char switches;
