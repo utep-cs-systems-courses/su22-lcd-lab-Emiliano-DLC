d2086c8cffd67b2d84d10da1bc8de386f4b5b56c
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-02 18:34:44 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -15,29 +15,7 @@ unsigned char step = 0;
 short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
 short colVelocity = 1, colLimits[2] = {1, screenWidth/2};
 
-void main()
-{
-  
-  P1DIR |= LED;		/**< Green led on when CPU on */
-  P1OUT |= LED;
-  configureClocks();
-  lcd_init();
-  switch_init();
-  
-  enableWDTInterrupts();      /**< enable periodic interrupt */
-  or_sr(0x8);	              /**< GIE (enable interrupts) */
-  
-  clearScreen(COLOR_BLUE);
-  while (1) {			/* forever */
-    if (redrawScreen) {
-      redrawScreen = 0;
-      update_shape();
-    }
-    P1OUT &= ~LED;	/* led off */
-    or_sr(0x10);	/**< CPU OFF */
-    P1OUT |= LED;	/* led on */
-  }
-}
+
 
 void
 screen_update_ball()
@@ -85,4 +63,28 @@ update_shape()
 {
   screen_update_ball();
   screen_update_hourglass();
+}
+
+void main()
+{
+  
+  P1DIR |= LED;		/**< Green led on when CPU on */
+  P1OUT |= LED;
+  configureClocks();
+  lcd_init();
+  switch_init();
+  
+  enableWDTInterrupts();      /**< enable periodic interrupt */
+  or_sr(0x8);	              /**< GIE (enable interrupts) */
+  
+  clearScreen(COLOR_BLUE);
+  while (1) {			/* forever */
+    if (redrawScreen) {
+      redrawScreen = 0;
+      update_shape();
+    }
+    P1OUT &= ~LED;	/* led off */
+    or_sr(0x10);	/**< CPU OFF */
+    P1OUT |= LED;	/* led on */
+  }
 }
