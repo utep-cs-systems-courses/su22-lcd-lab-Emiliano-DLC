4292a50e6b7a8ce75b22584e8483a7c7295c7b53
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-02 18:27:00 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,6 +12,9 @@
 char blue = 31, green = 0, red = 31;
 unsigned char step = 0;
 
+short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
+short colVelocity = 1, colLimits[2] = {1, screenWidth/2};
+
 void main()
 {
   
