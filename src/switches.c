63bd9feb8d2ef606313d3bae16a4be41d6570286
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-02 22:15:41 -0600

    Update switches.c

--- src/switches.c
+++ src/switches.c
@@ -30,3 +30,11 @@ void switch_interrupt_handler()
   char p2val = switch_update_interrupt_sense();
   switches = ~p2val & SWITCHES;
 }
+
+/* Switch on S2 */
+void __interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
+}
