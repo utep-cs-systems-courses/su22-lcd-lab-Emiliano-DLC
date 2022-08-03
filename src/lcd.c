b788566c5f9c31aaac5ec2cbc226cf3bca645171
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-03 11:02:50 -0600

    Update lcd.c

--- src/lcd.c
+++ src/lcd.c
@@ -72,7 +72,39 @@ void draw_ptrn()
 	buzzer_set_period(5000);
 }
 
+void draw_bar(int col, int row, unsigned short color)
+{
+  fillRectangle(col, row, 11, 1, color);
+}
 
+short barDrawPos[2] = {screenWidth/2, screenWidth/2}; // left bar on 0, right bar on 1
+short barControlPos[2] = {1+screenWidth/2, 1+screenWidth/2};
+short barLim[2] = {5,screenWidth-8};  //bar is of size 5 and bar have shifts of 3
+
+void screen_update_bar()
+{
+  for (char bar = 0; bar < 2; bar++) 
+    if (barDrawPos[bar] != barControlPos[bar]) // position changed?
+      goto redraw;
+  return;			// nothing to do
+ redraw:
+  draw_bar(barDrawPos[0]-5, 2, COLOR_BLACK);  // erase left bar
+  draw_bar(barDrawPos[1]-5, screenHeight-2, COLOR_BLACK);  // erase right bar
+  for (char bar = 0; bar < 2; bar++) 
+    barDrawPos[bar] = barControlPos[bar];
+  draw_bar(barDrawPos[0]-5, 2, COLOR_WHITE); // draw left bar
+  draw_bar(barDrawPos[1]-5, screenHeight-2, COLOR_WHITE); // draw right bar
+}
+
+void position_update_bar()
+{
+  if (switches & SW1 && barControlPos[1] > barLim[0]) barControlPos[1] -= 3;
+  if (switches & SW2 && barControlPos[1] < barLim[1]) barControlPos[1] += 3;
+  if (switches & SW3 && barControlPos[0] > barLim[0]) barControlPos[0] -= 3;
+  if (switches & SW4 && barControlPos[0] < barLim[1]) barControlPos[0] += 3;
+}
+
+----------------------------------------------------------------------------------
 
 char blue = 31, green = 0, red = 31;
 unsigned char step = 0;
@@ -104,6 +136,7 @@ void
 update_shape()
 {
   screen_update_ball();
+  screen_update_bar();
 	//led_start();
 	//draw_ptrn();
 	//R_draw_ptrn();
@@ -119,27 +152,10 @@ void wdt_c_handler()
 
   secCount ++;
   if (secCount >= 25) {		/* 10/sec */
-   
-    {				/* move ball */
-      short oldCol = controlPos[0];
-      short newCol = oldCol + colVelocity;
-      if (newCol <= colLimits[0] || newCol >= colLimits[1])
-	colVelocity = -colVelocity;
-      else
-	controlPos[0] = newCol;
-    }
-
-    {				/* update hourglass */
-      if (switches & SW3) green = (green + 1) % 64;
-      if (switches & SW2) blue = (blue + 2) % 32;
-      if (switches & SW1) red = (red - 3) % 32;
-      if (step <= 30)
-	step ++;
-      else
-	step = 0;
-      secCount = 0;
-    }
-    if (switches & SW4) return;
+  	position_update_ball();
+    position_update_bar();
     redrawScreen = 1;
+    secCount = 0;
+    }
+  
   }
-}
