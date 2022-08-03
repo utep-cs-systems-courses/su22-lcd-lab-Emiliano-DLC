ff9ed83c88865a44a2cdaee608c2806aad757fca
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-02 23:33:32 -0600

    Update lcd.c

--- src/lcd.c
+++ src/lcd.c
@@ -35,6 +35,32 @@ void R_draw_ptrn()
 	buzzer_set_period(0);
 }
 
+char blue = 31, green = 0, red = 31;
+unsigned char step = 0;
+
+short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
+short colVelocity = 1, colLimits[2] = {1, screenWidth/2};
+
+void
+draw_ball(int col, int row, unsigned short color)
+{
+  fillRectangle(col-1, row-1, 3, 3, color);
+}
+
+void
+screen_update_ball()
+{
+  for (char axis = 0; axis < 2; axis ++) 
+    if (drawPos[axis] != controlPos[axis]) /* position changed? */
+      goto redraw;
+  return;			/* nothing to do */
+ redraw:
+  draw_ball(drawPos[0], drawPos[1], COLOR_BLUE); /* erase */
+  for (char axis = 0; axis < 2; axis ++) 
+    drawPos[axis] = controlPos[axis];
+  draw_ball(drawPos[0], drawPos[1], COLOR_WHITE); /* draw */
+}
+
 void draw_ptrn()
 {
 	buzzer_set_period(5000);
