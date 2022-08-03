2059a180402472b68cc83ae7ad278db7bcdc5d3d
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-03 12:32:55 -0600

    Update lcd.c

--- src/lcd.c
+++ src/lcd.c
@@ -61,7 +61,7 @@ unsigned char step = 0;
 
 //short drawPos[2] = {screenHeight/2 , screenWidth/2}, controlPos[2] = {(screenHeight/2)+1, screenWidth/2};
 short drawPos[2] = {screenWidth/2 , screenHeight/2}, controlPos[2] = {(screenWidth/2)+1, screenHeight/2};
-short colVelocity = 1
+short colVelocity = 1;
 short colLimits[2] = {1, screenWidth};
 short rowLimits[2] = {1, screenHeight};
 
